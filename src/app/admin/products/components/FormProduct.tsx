2bdf17f3de35cacd5accacba5ccde684f21d7b4f
Thiago Calazans <devt.calazans@gmail.com>
2023-05-30 19:09:58 -0300

    Error Message

--- src/app/admin/products/components/FormProduct.tsx
+++ src/app/admin/products/components/FormProduct.tsx
@@ -2,7 +2,7 @@
 
 import { Form } from "@/components/ui/Form";
 import { useEffect, useState } from "react";
-import { Controller, useForm } from "react-hook-form";
+import { Controller, FormProvider, useForm } from "react-hook-form";
 import z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Button } from "@/components/ui/Button";
@@ -19,33 +19,36 @@ import { ErrorMessage } from "@/components/ui/Form/ErrorMessage";
 const createProductFormSchema = z.object({
   id: z.number().nullable().default(null),
   name: z.string().nonempty("Campo obrigatório"),
-  code: z.string(),
-  description: z.string(),
-  value: z.string().refine(
-    (valor) => {
-      // Expressão regular para verificar números, vírgulas, pontos e no máximo duas casas decimais
-      const regex = /^[\d,.]+(\.\d{1,2})?$/;
+  code: z.string().nonempty("Campo obrigatório"),
+  description: z.string().nonempty("Campo obrigatório"),
+  value: z
+    .string()
+    .nonempty("Campo obrigatório")
+    .refine(
+      (valor) => {
+        // Expressão regular para verificar números, vírgulas, pontos e no máximo duas casas decimais
+        const regex = /^[\d,.]+(\.\d{1,2})?$/;
 
-      // Verificar se a string corresponde à expressão regular
-      if (!regex.test(valor)) {
-        return false;
+        // Verificar se a string corresponde à expressão regular
+        if (!regex.test(valor)) {
+          return false;
+        }
+
+        // Verificar se não há mais de duas casas decimais
+        const partes = valor.replace(",", ".").split(".");
+        if (partes[1] && partes[1].length > 2) {
+          return false;
+        }
+
+        return true;
+      },
+      {
+        message:
+          "Apenas números, vírgulas, e no máximo duas casas decimais são permitidos.",
       }
-
-      // Verificar se não há mais de duas casas decimais
-      const partes = valor.replace(",", ".").split(".");
-      if (partes[1] && partes[1].length > 2) {
-        return false;
-      }
-
-      return true;
-    },
-    {
-      message:
-        "Apenas números, vírgulas, e no máximo duas casas decimais são permitidos.",
-    }
-  ),
-  brandId: z.string(),
-  categoryId: z.string(),
+    ),
+  brandId: z.string().nonempty("Campo obrigatório"),
+  categoryId: z.string().nonempty("Campo obrigatório"),
   trending: z.boolean(),
 });
 
@@ -61,15 +64,17 @@ export function FormProduct({ data, closeDialog }: FormProductProps) {
 
   const router = useRouter();
 
+  const createUseForm = useForm<ProductForm>({
+    resolver: zodResolver(createProductFormSchema),
+  });
+
   const {
     register,
     handleSubmit,
     formState: { errors },
     setValue,
     control,
-  } = useForm<ProductForm>({
-    resolver: zodResolver(createProductFormSchema),
-  });
+  } = createUseForm;
 
   useEffect(() => {
     if (data) {
@@ -119,93 +124,103 @@ export function FormProduct({ data, closeDialog }: FormProductProps) {
 
   return (
     <form className="flex flex-col gap-4" onSubmit={handleSubmit(onSubmit)}>
-      <div className="flex flex-col  items-start">
-        <Form.Control>
-          <Form.Label htmlFor="id">Id</Form.Label>
-          <Form.Input id="id" disabled register={register} />
-        </Form.Control>
-        <Form.Control className="w-full ">
-          <Form.Label htmlFor="code">Código</Form.Label>
-          <Form.Input
-            id="code"
-            type="text"
-            disabled={isLoading}
-            register={register}
-            required
-          />
-        </Form.Control>
-        <Form.Control className="w-full ">
-          <Form.Label htmlFor="name">Name</Form.Label>
-          <Form.Input
-            id="name"
-            type="text"
-            disabled={isLoading}
-            register={register}
-            required
-          />
-        </Form.Control>
-        <Form.Control className="w-full ">
-          <Form.Label htmlFor="description">Descrição</Form.Label>
-          <Form.Input
-            id="description"
-            type="text"
-            disabled={isLoading}
-            register={register}
-            required
-          />
-        </Form.Control>
-
-        <Form.Control className="w-full flex items-end justify-between gap-2">
-          <div className="flex-1 min-w-[8rem]">
-            <Form.Label htmlFor="value">Valor</Form.Label>
+      <FormProvider {...createUseForm}>
+        <div className="flex flex-col  items-start">
+          <Form.Control>
+            <Form.Label htmlFor="id">Id</Form.Label>
+            <Form.Input id="id" disabled register={register} />
+          </Form.Control>
+          <Form.Control className="w-full ">
+            <Form.Label htmlFor="code">Código</Form.Label>
             <Form.Input
-              id="value"
+              id="code"
               type="text"
               disabled={isLoading}
               register={register}
               required
             />
-          </div>
-          <Controller
-            name="brandId"
-            control={control}
-            render={({ field }) => <SelectBrands field={field} data={data} />}
-          />
-        </Form.Control>
-      </div>
-      <Controller
-        name="categoryId"
-        control={control}
-        render={({ field }) => <SelectCategories field={field} data={data} />}
-      />
-      <Controller
-        name="trending"
-        control={control}
-        render={({ field }) => (
-          <div className="flex items-center justify-end space-x-2">
-            <Checkbox
-              checked={field.value}
-              onCheckedChange={field.onChange}
-              id="trending"
-              className="peer h-4 w-4 shrink-0 rounded-full border border-zinc-300 ring-offset-zinc-900 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-zinc-900  data-[state=checked]:text-zinc-100"
+            <Form.ErrorMessage field="code" />
+          </Form.Control>
+          <Form.Control className="w-full ">
+            <Form.Label htmlFor="name">Name</Form.Label>
+            <Form.Input
+              id="name"
+              type="text"
+              disabled={isLoading}
+              register={register}
+              required
             />
-            <label
-              htmlFor="trending"
-              className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-            >
-              Trending
-            </label>
+            <Form.ErrorMessage field="name" />
+          </Form.Control>
+          <Form.Control className="w-full ">
+            <Form.Label htmlFor="description">Descrição</Form.Label>
+            <Form.Input
+              id="description"
+              type="text"
+              disabled={isLoading}
+              register={register}
+              required
+            />
+            <Form.ErrorMessage field="description" />
+          </Form.Control>
+
+          <Form.Control className="w-full flex items-end justify-between gap-2">
+            <div className="flex-1 min-w-[8rem]">
+              <Form.Label htmlFor="value">Valor</Form.Label>
+              <Form.Input
+                id="value"
+                type="text"
+                disabled={isLoading}
+                register={register}
+                required
+              />
+            </div>
+            <Controller
+              name="brandId"
+              control={control}
+              render={({ field }) => <SelectBrands field={field} data={data} />}
+            />
+          </Form.Control>
+          <div className="w-full flex items-end justify-between gap-2">
+            <Form.ErrorMessage field="value" />
+            <Form.ErrorMessage field="brandId" />
           </div>
-        )}
-      />
-      <Button
-        className="mt-2"
-        type="submit"
-        variant="primary"
-        disabled={isLoading}
-      >
-        Register
-      </Button>
+        </div>
+        <Controller
+          name="categoryId"
+          control={control}
+          render={({ field }) => <SelectCategories field={field} data={data} />}
+        />
+        <Form.ErrorMessage field="categoryId" />
+        <Controller
+          name="trending"
+          control={control}
+          render={({ field }) => (
+            <div className="flex items-center justify-end space-x-2">
+              <Checkbox
+                checked={field.value}
+                onCheckedChange={field.onChange}
+                id="trending"
+                className="peer h-4 w-4 shrink-0 rounded-full border border-zinc-300 ring-offset-zinc-900 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-zinc-900  data-[state=checked]:text-zinc-100"
+              />
+              <label
+                htmlFor="trending"
+                className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+              >
+                Trending
+              </label>
+            </div>
+          )}
+        />
+        <Button
+          className="mt-2"
+          type="submit"
+          variant="primary"
+          disabled={isLoading}
+        >
+          Register
+        </Button>
+      </FormProvider>
     </form>
   );
 }
